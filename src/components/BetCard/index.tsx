d44b21aabb093d989a73ee594ded680091cfe7d4
stanleyamate <amatestanley08@gmail.com>
2024-09-26 19:25:38 +0100

    removed main from card

--- src/components/BetCard/index.tsx
+++ src/components/BetCard/index.tsx
@@ -16,21 +16,19 @@ const BetCard: FC<Props> = (props) => {
   const { stars, betAmount, getAmount, title, logo } = props;
 
   return (
-    <div className="grid bg-gray-200 items-center px-3 min-h-screen">
-      <div className='grid w-full grid-cols-1 gap-8 md:gap-5 md:grid-cols-2 lg:grid-rows-1 md:grid-rows-3  lg:grid-cols-4 xl:grid-cols-6 bg-white shadow-lg p-3'>
-        <div className="bg-primary grid md:col-span-1 md:row-span-2 xl:col-span-2 lg:row-span-1 items-center justify-center ">
-          <img src={logo} alt="logo" />
-        </div>
-        <div className="flex  flex-col items-center justify-center md:items-start gap-4 lg:flex-col lg:items-center">
-          <h2 className="text-xl underline hover:no-underline">{title}</h2>
-          <Rating stars={stars} />
-        </div>
+    <div className='grid w-full grid-cols-1 gap-8 md:gap-5 md:grid-cols-2 lg:grid-rows-1 md:grid-rows-3  lg:grid-cols-4 xl:grid-cols-6 bg-white shadow-lg p-3'>
+      <div className="bg-primary grid md:col-span-1 md:row-span-2 xl:col-span-2 lg:row-span-1 items-center justify-center ">
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="flex  flex-col items-center justify-center md:items-start gap-4 lg:flex-col lg:items-center">
+        <h2 className="text-xl underline hover:no-underline">{title}</h2>
+        <Rating stars={stars} />
+      </div>
 
-        <Bunus betAmount={betAmount} getAmount={getAmount} />
+      <Bunus betAmount={betAmount} getAmount={getAmount} />
 
-        <div className='flex-1 md:col-span-2 lg:col-span-1 xl:col-span-2  xl:pr-12 flex items-center md:items-end lg:items-center justify-center'>
-          <Button size='lg' fullWidth>Bet Now</Button>
-        </div>
+      <div className='flex-1 md:col-span-2 lg:col-span-1 xl:col-span-2  xl:pr-12 flex items-center md:items-end lg:items-center justify-center'>
+        <Button size='lg' fullWidth>Bet Now</Button>
       </div>
     </div>
   )
